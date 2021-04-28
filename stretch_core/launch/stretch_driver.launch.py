58ccc610a50647667d8dd97d4b793709f6265e21
David V. Lu!! <davidvlu@gmail.com>
2021-04-28 06:24:53 -0400

    Launch File Fixes (#22)

--- stretch_core/launch/stretch_driver.launch.py
+++ stretch_core/launch/stretch_driver.launch.py
@@ -1,9 +1,13 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
 import xacro
 
 
@@ -21,7 +25,7 @@ def generate_launch_description():
                                  name='joint_state_publisher',
                                  arguments=[robot_description_path],
                                  output='log',
-                                 parameters=[{'source_list': ["/stretch/joint_states"]},
+                                 parameters=[{'source_list': ['/stretch/joint_states']},
                                              {'rate': 15}])
 
     robot_state_publisher = Node(package='robot_state_publisher',
@@ -30,6 +34,24 @@ def generate_launch_description():
                                  output='both',
                                  parameters=[{'robot_description': xacro.process_file(robot_description_path).toxml()},
                                              {'publish_frequency': 15.0}])
+
+    declare_broadcast_odom_tf_arg = DeclareLaunchArgument(
+        'broadcast_odom_tf',
+        default_value=str(False),
+        description='Whether to broadcast the odom TF'
+    )
+
+    declare_fail_out_of_range_goal_arg = DeclareLaunchArgument(
+        'fail_out_of_range_goal',
+        default_value=str(True),
+        description='Whether the motion action servers fail on out-of-range commands'
+    )
+
+    declare_mode_arg = DeclareLaunchArgument(
+        'mode',
+        default_value=str('position'),
+        description='The mode in which the ROS driver commands the robot'
+    )
 
     stretch_driver = Node(package='stretch_core',
                           executable='stretch_driver',
@@ -40,10 +62,12 @@ def generate_launch_description():
                                       {'timeout': 0.5},
                                       {'controller_calibration_file': calibrated_controller_yaml_file},
                                       {'broadcast_odom_tf': LaunchConfiguration('broadcast_odom_tf')},
-                                      {'fail_out_of_range_goal': LaunchConfiguration('fail_out_of_range_goal')}])
+                                      {'fail_out_of_range_goal': LaunchConfiguration('fail_out_of_range_goal')},
+                                      {'mode': LaunchConfiguration('mode')}])
 
-    return LaunchDescription([DeclareLaunchArgument('broadcast_odom_tf'),
-                              DeclareLaunchArgument('fail_out_of_range_goal'),
+    return LaunchDescription([declare_broadcast_odom_tf_arg,
+                              declare_fail_out_of_range_goal_arg,
+                              declare_mode_arg,
                               joint_state_publisher,
                               robot_state_publisher,
                               stretch_driver])
