5af9a7479c4637fef171844ceccf5aba7a050c43
Vatan Aksoy Tezer <vatan@picknik.ai>
2021-05-10 12:52:13 +0000

    base of sdf

--- stretch_gazebo/launch/ignition.launch.py
+++ stretch_gazebo/launch/ignition.launch.py
@@ -6,6 +6,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
@@ -30,6 +31,11 @@ def load_yaml(package_name, file_path):
 
 def generate_launch_description():
 
+    # Launch Arguments
+    use_sim_time = DeclareLaunchArgument(
+        "use_sim_time", default_value="False", description="If true, use simulated clock"
+    )
+
     # Ignition gazebo
     pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     gazebo = IncludeLaunchDescription(
@@ -48,7 +54,7 @@ def generate_launch_description():
     )
 
     # Spawn
-    gazebo_pkg_path =  os.path.join(get_package_share_directory("stretch_gazebo"), "urdf", "stretch.sdf")
+    gazebo_pkg_path =  os.path.join(get_package_share_directory("stretch_gazebo"), "models", "stretch_ignition", "model.sdf")
     spawn = Node(package='ros_ign_gazebo', executable='create',
                 arguments=[
                     '-name', 'robot',
@@ -56,17 +62,41 @@ def generate_launch_description():
                     ],
                 output='screen')
 
+    # Ign Bridge
     bridge = Node(
         package='ros_ign_bridge',
         executable='parameter_bridge',
         arguments=['/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
-                '/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry'],
+                '/model/robot/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
+                '/model/robot/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+                '/clock@rosgraph_msgs/msg/Clock@ignition.msgs.Clock'
+                ],
+        remappings=[
+            ("/model/robot/tf", "tf"),
+            ("/model/robot/odometry", "odom")
+        ],
         output='screen'
     )
 
+    # Robot state publisher
+    # robot_desc_path =  os.path.join(get_package_share_directory("stretch_gazebo"), "urdf", "stretch_gazebo.urdf.xacro")
+    # robot_description_config = xacro.process_file(
+    #     robot_desc_path
+    # )
+    # robot_description = {"robot_description": robot_description_config.toxml()}
+    # robot_state_publisher = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     name="robot_state_publisher",
+    #     output="both",
+    #     parameters=[],
+    # )
+
     return LaunchDescription([
+        use_sim_time,
         gazebo,
         spawn,
         bridge,
         rviz,
+        # robot_state_publisher,
     ])
