88c9d1be4e926262f3170339fdcc2440d76c0c3d
David V. Lu <davidvlu@gmail.com>
2021-05-13 16:01:24 -0400

    Minor launch linting

--- stretch_moveit_config/launch/demo.launch.py
+++ stretch_moveit_config/launch/demo.launch.py
@@ -1,12 +1,18 @@
-import os
-import yaml
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
-from ament_index_python.packages import get_package_share_directory
-import xacro
 import argparse
+import os
 import sys
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+from launch_ros.actions import Node
+
+import xacro
+
+import yaml
+
 
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
@@ -15,8 +21,9 @@ def load_file(package_name, file_path):
     try:
         with open(absolute_file_path, 'r') as file:
             return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
+
 
 def load_yaml(package_name, file_path):
     package_path = get_package_share_directory(package_name)
@@ -25,7 +32,7 @@ def load_yaml(package_name, file_path):
     try:
         with open(absolute_file_path, 'r') as file:
             return yaml.safe_load(file)
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
         return None
 
 
@@ -37,6 +44,7 @@ CONFIGURATION_TRANSLATION = {
     'base': ['position/x', 'position/theta'],
     'arm': ['joint_arm_l0', 'joint_arm_l1', 'joint_arm_l2', 'joint_arm_l3']
 }
+
 
 def load_joint_limits_from_config(mode='default'):
     """Translate the values from the robot configuration to params to be used by MoveIt."""
@@ -68,19 +76,21 @@ def load_joint_limits_from_config(mode='default'):
 
 def generate_launch_description():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--use_fake_controller", default=False, type=eval, choices=[True, False])
+    parser.add_argument('--use_fake_controller', default=False, type=eval, choices=[True, False])
     args, _ = parser.parse_known_args([arg for sys_arg in sys.argv[4:] for arg in ('--' + sys_arg).split(':=')])
 
     ld = LaunchDescription()
+
+    package_dir = get_package_share_directory('stretch_moveit_config')
     # planning_context
-    robot_description_config = xacro.process_file(os.path.join(get_package_share_directory('stretch_moveit_config'),
+    robot_description_config = xacro.process_file(os.path.join(package_dir,
                                                                'config',
                                                                'stretch.xacro'),
-                                                  mappings={"use_fake_controller": str(args.use_fake_controller)})
+                                                  mappings={'use_fake_controller': str(args.use_fake_controller)})
     robot_description = {'robot_description': robot_description_config.toxml()}
 
     robot_description_semantic_config = load_file('stretch_moveit_config', 'config/stretch_description.srdf')
-    robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
+    robot_description_semantic = {'robot_description_semantic': robot_description_semantic_config}
 
     kinematics_yaml = load_yaml('stretch_moveit_config', 'config/kinematics.yaml')
     sensors_yaml = load_yaml('stretch_moveit_config', 'config/sensors_3d.yaml')
@@ -88,27 +98,27 @@ def generate_launch_description():
     joint_limits_yaml = {'robot_description_planning': load_joint_limits_from_config()}
 
     # Planning Functionality
-    ompl_planning_pipeline_config = { 'move_group' : {
-        'planning_plugin' : 'ompl_interface/OMPLPlanner',
-        'request_adapters' : """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""" ,
-        'start_state_max_bounds_error' : 0.1 } }
+    ompl_planning_pipeline_config = {'move_group': {
+        'planning_plugin': 'ompl_interface/OMPLPlanner',
+        'request_adapters': """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+        'start_state_max_bounds_error': 0.1}}
     ompl_planning_yaml = load_yaml('stretch_moveit_config', 'config/ompl_planning.yaml')
     ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
 
     # Trajectory Execution Functionality
     controllers_yaml = load_yaml('stretch_moveit_config', 'config/moveit_simple_controllers.yaml')
-    moveit_controllers = { 'moveit_simple_controller_manager' : controllers_yaml,
-                           'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
+    moveit_controllers = {'moveit_simple_controller_manager': controllers_yaml,
+                          'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
 
     trajectory_execution = {'moveit_manage_controllers': True,
                             'trajectory_execution.allowed_execution_duration_scaling': 1.2,
                             'trajectory_execution.allowed_goal_duration_margin': 0.5,
                             'trajectory_execution.allowed_start_tolerance': 0.01}
 
-    planning_scene_monitor_parameters = {"publish_planning_scene": True,
-                                         "publish_geometry_updates": True,
-                                         "publish_state_updates": True,
-                                         "publish_transforms_updates": True}
+    planning_scene_monitor_parameters = {'publish_planning_scene': True,
+                                         'publish_geometry_updates': True,
+                                         'publish_state_updates': True,
+                                         'publish_transforms_updates': True}
 
     # Start the actual move_group node/action server
     run_move_group_node = Node(package='moveit_ros_move_group',
@@ -126,7 +136,7 @@ def generate_launch_description():
     ld.add_action(run_move_group_node)
 
     # RViz
-    rviz_config_file = get_package_share_directory('stretch_moveit_config') + "/launch/moveit.rviz"
+    rviz_config_file = package_dir + '/launch/moveit.rviz'
     rviz_node = Node(package='rviz2',
                      executable='rviz2',
                      name='rviz2',
@@ -158,16 +168,16 @@ def generate_launch_description():
         fake_joint_driver_node = Node(
             package='controller_manager',
             executable='ros2_control_node',
-            parameters=[robot_description,  os.path.join(get_package_share_directory("stretch_moveit_config"), "config", "ros_controllers.yaml")],
+            parameters=[robot_description, os.path.join(package_dir, 'config', 'ros_controllers.yaml')],
         )
         ld.add_action(fake_joint_driver_node)
 
-        for controller in ["stretch_controller", "joint_state_controller"]:
+        for controller in ['stretch_controller', 'joint_state_controller']:
             ld.add_action(
                 ExecuteProcess(
-                    cmd=["ros2 run controller_manager spawner.py {}".format(controller)],
+                    cmd=['ros2 run controller_manager spawner.py {}'.format(controller)],
                     shell=True,
-                    output="screen",
+                    output='screen',
                 )
             )
 
