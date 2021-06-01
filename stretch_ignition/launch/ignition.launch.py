f50eb5e1ddb5652b25df1cd1a42be488b855669f
Vatan Aksoy Tezer <vatan@picknik.ai>
2021-06-01 18:24:15 +0000

    Add xacro spawn and port sensors

--- stretch_ignition/launch/ignition.launch.py
+++ stretch_ignition/launch/ignition.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    robot_description_path =  os.path.join(get_package_share_directory("stretch_description"), "urdf", "stretch_description.xacro")
+    robot_description_path =  os.path.join(get_package_share_directory("stretch_description"), "urdf", "stretch_ignition.xacro")
     robot_description_config = xacro.process_file(
         robot_description_path
     )
@@ -72,11 +72,10 @@ def generate_launch_description():
     )
 
     # Spawn
-    stretch_sdf_path =  os.path.join(get_package_share_directory("stretch_ignition"), "sdf", "stretch.sdf")
     spawn = Node(package='ros_ign_gazebo', executable='create',
                 arguments=[
                     '-name', 'stretch',
-                    '-file', stretch_sdf_path,
+                    '-topic', 'robot_description',
                     '-z', '0.1',
                     ],
                 output='screen',
@@ -103,8 +102,8 @@ def generate_launch_description():
                 # JointTrajectoryProgress bridge (IGN -> ROS2)
                 '/joint_trajectory_progress@std_msgs/msg/Float32[ignition.msgs.Float',
                 # Lidar (IGN -> ROS2)
-                '/lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
-                '/lidar/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+                '/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+                '/scan/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
                 # Base IMU (IGN -> ROS2)
                 '/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
                 # Base Magnetometer (IGN -> ROS2)
@@ -113,24 +112,43 @@ def generate_launch_description():
                 '/wrist_imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
                 # Realsense IMU
                 '/realsense_imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
-                # RGBD Camera (TODO: Port realsense) (IGN -> ROS2)
-                '/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/image@sensor_msgs/msg/Image[ignition.msgs.Image',
-                '/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
-                '/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
-                '/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/camera_info@sensor_msgs/msg/CameraInfo[ignition::msgs::CameraInfo',
+                # Realsense Camera (TODO: Port realsense plugin when ign supports custom plugins) (IGN -> ROS2)
+                # Realsense Color
+                '/world/default/model/stretch/link/camera_color_optical_frame/sensor/realsense_d435_color/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+                '/world/default/model/stretch/link/camera_color_optical_frame/sensor/realsense_d435_color/camera_info@sensor_msgs/msg/CameraInfo[ignition::msgs::CameraInfo',
+                # Realsense Depth
+                '/world/default/model/stretch/link/camera_depth_optical_frame/sensor/realsense_d435/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+                '/world/default/model/stretch/link/camera_depth_optical_frame/sensor/realsense_d435/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+                '/world/default/model/stretch/link/camera_depth_optical_frame/sensor/realsense_d435/camera_info@sensor_msgs/msg/CameraInfo[ignition::msgs::CameraInfo',
+                # Realsense IR 1
+                '/world/default/model/stretch/link/camera_infra1_optical_frame/sensor/realsense_d435_ir/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+                '/world/default/model/stretch/link/camera_infra1_optical_frame/sensor/realsense_d435_ir/camera_info@sensor_msgs/msg/CameraInfo[ignition::msgs::CameraInfo',                
+                # Realsense IR 2
+                '/world/default/model/stretch/link/camera_infra2_optical_frame/sensor/realsense_d435_ir2/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+                '/world/default/model/stretch/link/camera_infra2_optical_frame/sensor/realsense_d435_ir2/camera_info@sensor_msgs/msg/CameraInfo[ignition::msgs::CameraInfo',  
                 ],
         remappings=[
             ("/model/stretch/tf", "tf"),
             ("/world/default/model/stretch/joint_state", "joint_states"),
-            ("/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/image", "/realsense/color/image_raw"),
-            ("/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/camera_info", "/realsense/depth/camera_info"),
-            ("/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/points", "/realsense/depth/points"),
-            ("/world/default/model/stretch/link/link_realsense_optical/sensor/realsense_d435/depth_image", "/realsense/depth/image_raw"),
             ("/model/stretch/odometry", "odom"),
             ("/imu", "imu/data"),
             ("/magnetometer", "mag"),
             ("/wrist_imu", "wrist_imu/data"),
+            # Realsense IMU
             ("/realsense_imu", "realsense/imu/data"),
+            # Realsense Color
+            ("/world/default/model/stretch/link/camera_color_optical_frame/sensor/realsense_d435_color/image", "/realsense/color/image_raw"),
+            ("/world/default/model/stretch/link/camera_color_optical_frame/sensor/realsense_d435_color/camera_info", "/realsense/color/camera_info"),
+            # Realsense Depth
+            ("/world/default/model/stretch/link/camera_depth_optical_frame/sensor/realsense_d435/camera_info", "/realsense/depth/camera_info"),
+            ("/world/default/model/stretch/link/camera_depth_optical_frame/sensor/realsense_d435/points", "/realsense/depth/points"),
+            ("/world/default/model/stretch/link/camera_depth_optical_frame/sensor/realsense_d435/depth_image", "/realsense/depth/image_raw"),
+            # Realsense IR 1
+            ("/world/default/model/stretch/link/camera_infra1_optical_frame/sensor/realsense_d435_ir/image", "realsense/infrared/image_raw"),
+            ("/world/default/model/stretch/link/camera_infra1_optical_frame/sensor/realsense_d435_ir/camera_info", "realsense/infrared/camera_info"),
+            # Realsense IR 2
+            ("/world/default/model/stretch/link/camera_infra2_optical_frame/sensor/realsense_d435_ir2/image", "realsense/infrared2/image_raw"),
+            ("/world/default/model/stretch/link/camera_infra2_optical_frame/sensor/realsense_d435_ir2/camera_info", "realsense/infrared2/camera_info"),
         ],
         output='screen'
     )
@@ -140,7 +158,7 @@ def generate_launch_description():
                         executable='static_transform_publisher',
                         name='lidar_static_transform_publisher',
                         output='log',
-                        arguments=['0', '0.0', '0.1664', '0.0', '0.0', '0.0', 'base_link', 'stretch/link_laser/gpu_lidar'])
+                        arguments=['0', '0.0', '0.0', '0.0', '0.0', '0.0', 'laser', 'stretch/laser/gpu_lidar'])
     imu_static_tf = Node(package='tf2_ros',
                         executable='static_transform_publisher',
                         name='imu_static_transform_publisher',
@@ -160,15 +178,27 @@ def generate_launch_description():
                         executable='static_transform_publisher',
                         name='realsense_imu_static_transform_publisher',
                         output='log',
-                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'link_head_tilt', 'stretch/link_head_tilt/realsense_imu'])
-    # TODO (vatanaksoytezer): Port realsense and remove rgbd
-    rgbd_static_tf = Node(package='tf2_ros',
+                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'camera_gyro_frame', 'stretch/camera_gyro_frame/realsense_imu'])
+    realsense_color_static_tf = Node(package='tf2_ros',
                         executable='static_transform_publisher',
                         name='rgbd_static_transform_publisher',
                         output='log',
-                        # arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'camera_depth_optical_frame', 'stretch/camera_depth_optical_frame/rgbd_camera'])
-                        arguments=['0.0', '0.0', '0.0', '1.5708', '-1.5708', '0', 'camera_depth_optical_frame', 'stretch/link_realsense_optical/realsense_d435'])
-                        # arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'link_head_tilt', 'stretch/link_head_tilt/rgbd_camera'])
+                        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'camera_color_optical_frame', 'stretch/camera_color_optical_frame/realsense_d435_color'])
+    realsense_depth_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='rgbd_static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'camera_depth_optical_frame', 'stretch/camera_depth_optical_frame/realsense_d435'])
+    realsense_ir_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='rgbd_static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'camera_infra1_optical_frame', 'stretch/camera_infra1_optical_frame/realsense_d435_ir'])
+    realsense_ir2_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='rgbd_static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'camera_infra2_optical_frame', 'stretch/camera_infra2_optical_frame/realsense_d435_ir2'])
 
     # Controllers 
     # TODO (vatanaksoytezer): Use ros_ign_control when it is ready
@@ -196,8 +226,11 @@ def generate_launch_description():
             imu_static_tf,
             mag_static_tf,
             wrist_imu_static_tf,
-            rgbd_static_tf,
             realsense_imu_static_tf,
+            realsense_color_static_tf,
+            realsense_depth_static_tf,
+            realsense_ir_static_tf,
+            realsense_ir2_static_tf,
             rviz,
         ]
     )
